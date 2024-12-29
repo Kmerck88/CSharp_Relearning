9bc8b02200f97d103cc0f354b77a14a0926e9508
Kmerck88 <kevin.merck88@gmail.com>
2024-12-29 11:22:18 -0500

    Refactor number input and addition functionality
    Removed `Program.cs` and migrated functionality to
    `AddingNumberToFloat.cs`. Added new functionality in
    `AddingTwoFloats.cs` to handle two number inputs and
    calculate their sum. Created `AddingTwoFloats.csproj`
    with necessary project settings.

---
+++ CSharp_Basics/Numbers/AddingTwoFloats/AddingTwoFloats.cs
@@ -0,0 +1,16 @@
+﻿//Prompt the user to enter two numbers. After the user enters the numbers, add them together and output the sum back to the console.
+
+//Example
+//>>Enter a number: << 12.2
+//>> Enter another number: << 17.3
+//>> The sum is 29.3
+
+Console.WriteLine("Enter a number: ");
+double number1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter another number: ");
+double number2 = Convert.ToDouble(Console.ReadLine());
+double sum = number1 + number2;
+Console.WriteLine("The sum is " + sum);
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
